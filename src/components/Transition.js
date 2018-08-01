bdb417cca2aaab56119691f8f8900cd8e736043b
JP <johnny.phocker@gmail.com>
2018-08-01 16:56:36 -0600

    Primer commit

---
+++ src/components/Transition.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition';
+
+
+import '../css/App.css';
+
+class TransitionComp extends Component{
+
+	state = {
+		show: true
+	}
+	showDiv = () => {
+		this.setState({
+			show: !this.state.show ? true : false
+		})
+	}
+
+    render(){
+        return(
+            <div>
+            	<Transition in={this.state.show} timeout={2000} mountOnEnter unmountOnExit>
+            		{ state =>
+            			<div style={{
+            				background: 'red',
+            				height: '100px',
+            				transition: 'all 2s ease',
+            				opacity: state === 'exited' || state === 'exiting' ? 0 : 1
+            			}}>
+            			
+            			</div>
+            		}
+            	</Transition>
+               
+                <div className='showDiv' onClick={this.showDiv}>Click</div>
+            </div>
+
+        )
+    }
+}
+
+
+export default TransitionComp;
